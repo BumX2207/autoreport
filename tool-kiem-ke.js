f99cc0e0722762ddc46185d199961ca4916e6837
BumX2207 <congtunho209@gmail.com>
2026-01-30 21:54:28 +0700

    Update tool-kiem-ke.js

--- tool-kiem-ke.js
+++ tool-kiem-ke.js
@@ -685,7 +685,7 @@
     };
 
     return {
-        name: "Kiểm kê V2.7 (Pro)",
+        name: "Kiểm kê V1.0",
         icon: `<svg viewBox="0 0 24 24"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z" fill="white"/></svg>`,
         bgColor: "#6c757d",
         css: MY_CSS,
