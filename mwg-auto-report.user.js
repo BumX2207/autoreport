a0b45d446c03687cfde4f44da81522bd519d7578
BumX2207 <congtunho209@gmail.com>
2026-02-06 15:53:46 +0700

    Update mwg-auto-report.user.js

--- mwg-auto-report.user.js
+++ mwg-auto-report.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Auto BI 6.6
+// @name         Auto BI 6.7
 // @namespace    http://tampermonkey.net/
-// @version      6.6
+// @version      6.7
 // @description  Tool báo cáo tự động cho quản lý MWG.
 // @author       42060
 // @match        https://bi.thegioididong.com/*
