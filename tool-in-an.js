73c2bee46b89f01d1e95a5749931ad5e62350dd7
BumX2207 <congtunho209@gmail.com>
2026-02-06 22:22:44 +0700

    Update tool-in-an.js

--- tool-in-an.js
+++ tool-in-an.js
@@ -155,12 +155,12 @@
     // ===============================================================
     const runTool = () => {
 
-       const ALLOWED_USERS = ["User1", "User2"]; // <--- Điền user vào đây
+       const ALLOWED_USERS = ["42060 - Ngô Hữu Thọ"]; // <--- Điền user vào đây
         const currentUser = AUTH_STATE?.userName || "---";
         if (ALLOWED_USERS.length > 0) {
             const isAllowed = ALLOWED_USERS.some(u => u.trim().toLowerCase() === currentUser.trim().toLowerCase());
             if (!isAllowed) {
-                if (UI.showToast) UI.showToast(`⛔ Bạn (${currentUser}) chưa được cấp quyền!`);
+                if (UI.showToast) UI.showToast(`⛔ Người dùng: ${currentUser} chưa được cấp quyền!`);
                 return; // Ngắt code ngay lập tức
             }
         }
