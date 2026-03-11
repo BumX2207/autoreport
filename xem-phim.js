d602e51a8bc9b70091be547a624452bc2cde1920
BumX2207 <congtunho209@gmail.com>
2026-03-11 23:54:00 +0700

    Update xem-phim.js

--- xem-phim.js
+++ xem-phim.js
@@ -573,7 +573,7 @@
     };
     
     return {
-        name: "Xem Phim",
+        name: "Xem Phim V1",
         icon: `<svg viewBox="0 0 24 24"><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-2zm-6.75 11.25L10 12l4.25 3.25L10 18v-2.75z" fill="white"/></svg>`,
         bgColor: "#d63031",
         action: runTool
