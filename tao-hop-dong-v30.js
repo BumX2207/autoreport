6baf9e6d9d847913becff221b90896076dd3f355
BumX2207 <congtunho209@gmail.com>
2026-07-30 20:23:20 +0700

    Update tao-hop-dong-v30.js

--- tao-hop-dong-v30.js
+++ tao-hop-dong-v30.js
@@ -1794,7 +1794,7 @@
     };
 
     return {
-        name: "Tạo Hợp Đồng V1",
+        name: "Tạo Hợp Đồng",
         icon: `<svg viewBox="0 0 24 24" fill="currentColor"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 15H7v-2h10v2zm0-4H7v-2h10v2zm0-4H7V7h10v2z"/></svg>`,
         bgColor: "#6c5ce7",
         action: runTool
